35467fdbb6a9be54eda736ca78e7b935b7e41f42
Mangesh Prakash Jadhav <42151779+mangesh1409@users.noreply.github.com>
2019-09-25 14:00:41 +0530

    Add files via upload

---
+++ Pattern9.c
@@ -0,0 +1,40 @@
+/*  Accept number from user and display below pattern
+    Input= 5							 Input= 4	               
+    Output= 1 2 3 4 5   		      Output=  1 2 3 4
+				1 2 3 4 5						  1 2 3 4
+				1 2 3 4 5						  1 2 3 4
+				1 2 3 4 5						  1 2 3 4
+				1 2 3 4 5                                                              */
+				
+#include<stdio.h>
+
+void Pattern(int iNo)
+{
+	int iRow=0,  iCol=0;
+	
+	if(iNo<0)
+	{
+		iNo=-iNo;
+	}
+	
+	for(iRow=1;iRow<=iNo;iRow++)
+	{
+		for(iCol=1;iCol<=iNo;iCol++)
+		{
+				printf("%d ",iCol);
+		}
+		printf("\n");
+	}
+}
+
+int main()
+{
+	int iValue=0;
+	printf("Enter number : ");
+	scanf("%d",&iValue);
+	
+	Pattern(iValue);
+	printf("\n");
+	
+	return 0;
+}
