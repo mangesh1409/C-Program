67e909130f71922623f68dcb54fc6af4b2b87101
Mangesh Prakash Jadhav <42151779+mangesh1409@users.noreply.github.com>
2019-09-25 15:07:28 +0530

    Update Pattern5.c

--- Pattern5.c
+++ Pattern5.c
@@ -1,6 +1,6 @@
 //  Accept number from user and display below pattern
-//  Input= 6									               Input= 8
-//  Output=  1 2 3 4 5 6 5 4 3 2 1 				Output=  1 2 3 4 5 6 7 8 7 6 5 4 3 2 1
+//  Input= 6		               Input= 8
+//  Output=  1 2 3 4 5 6 5 4 3 2 1     Output=  1 2 3 4 5 6 7 8 7 6 5 4 3 2 1
 
 #include<stdio.h>
 
