fbb66273c6a33c88981d5501f249c3de1268be39
Mangesh Prakash Jadhav <42151779+mangesh1409@users.noreply.github.com>
2019-09-27 01:14:01 +0530

    Update Pattern27.c

--- Pattern27.c
+++ Pattern27.c
@@ -1,10 +1,10 @@
 /*  Accept number of rows and columns from user and display below pattern
     Input= 	iRow=5   iCol=5
-    Output=   A A A A A
-    				 B B B B B
+    Output=   			 A A A A A
+    	       			 B B B B B
     				 C C C C C
     				 D D D D D
-    				 E E E E E     				   		                                                               */
+    				 E E E E E                                  */
 				
 #include<stdio.h>
 
