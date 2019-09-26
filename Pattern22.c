6824c56a1a2e9238f93cef126b1bd29d811c60d1
Mangesh Prakash Jadhav <42151779+mangesh1409@users.noreply.github.com>
2019-09-27 00:59:10 +0530

    Update Pattern22.c

--- Pattern22.c
+++ Pattern22.c
@@ -1,10 +1,10 @@
 /*  Accept number of rows and columns from user and display below pattern
     Input= 	iRow=5   iCol=5
-    Output=  1     2    3    4    5
-                    6     7    8    9   10
-                   11  12  13  14  15
-                   16  17  18  19  20
-                   21   22  23 24  25          				   		                                                               */
+    Output=  1   2   3   4   5
+             6   7   8   9   10
+             11  12  13  14  15
+             16  17  18  19  20
+             21  22  23  24  25          				   		                                                               */
 				
 #include<stdio.h>
 
