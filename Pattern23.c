592b23f6e10fa7c70dc5f5df62c3f533570a06a3
Mangesh Prakash Jadhav <42151779+mangesh1409@users.noreply.github.com>
2019-09-27 00:59:33 +0530

    Update Pattern23.c

--- Pattern23.c
+++ Pattern23.c
@@ -1,6 +1,7 @@
 /*  Accept number of rows and columns from user and display below pattern
     Input= 	iRow=5   iCol=5
-    Output=   * * * * *
+    Output=  
+    			   * * * * *
     		           $ $ $ $ $
     		           $ $ $ $ $
     		           $ $ $ $ $
