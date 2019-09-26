988742aa13c0a97dd562f119874609c4a18a8436
Mangesh Prakash Jadhav <42151779+mangesh1409@users.noreply.github.com>
2019-09-27 00:54:52 +0530

    Update Pattern14.c

--- Pattern14.c
+++ Pattern14.c
@@ -1,6 +1,7 @@
 /*  Accept number of rows and columns from user and display below pattern
     Input= 	iRow=5   iCol=4
-    Output= 1 2 3 4 
+    Output= 
+    				1 2 3 4 
     				* * * *
     				1 2 3 4 
     				* * * *
