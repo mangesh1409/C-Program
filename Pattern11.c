da277705b06623eb122dc71636338508e8106d81
Mangesh Prakash Jadhav <42151779+mangesh1409@users.noreply.github.com>
2019-09-27 00:53:29 +0530

    Update Pattern11.c

--- Pattern11.c
+++ Pattern11.c
@@ -1,10 +1,10 @@
 /*  Accept number from user and display below pattern
     Input= 5								               
     Output= 5 5 5 5 5
-    				4 4 4 4 4
-    				3 3 3 3 3
-    				2 2 2 2 2	
-    				1 1 1 1 1   		                                                               */
+    	    4 4 4 4 4
+    	    3 3 3 3 3
+    	    2 2 2 2 2	
+    	    1 1 1 1 1                                   */
 				
 #include<stdio.h>
 
