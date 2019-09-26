6d7b7762a6837a6d133e5aeab0443ff8f9836b6e
Mangesh Prakash Jadhav <42151779+mangesh1409@users.noreply.github.com>
2019-09-27 01:42:23 +0530

    Add files via upload

---
+++ Pattern30.c
@@ -0,0 +1,40 @@
+/*  Accept number of rows and columns from user and display below pattern
+    Input= 	iRow=5   iCol=5
+    Output=   a b c d e
+    				 a b c d e
+    				 a b c d e
+    				 a b c d e
+    				 a b c d e     				   		                                                               */
+				
+#include<stdio.h>
+
+void Pattern(int iRow,int iCol)
+{
+	int i=0,j=0;
+	char ch='\0';
+	
+	for(i=1;i<=iRow;i++)
+	{
+		for(j=1,ch='a';j<=iCol;j++)
+		{
+			printf("%c ",ch);
+			ch++;
+		}
+		printf("\n");
+	}
+}
+
+int main()
+{
+	int iValue1=0, iValue2=0;
+	printf("Enter number number of rows : ");
+	scanf("%d",&iValue1);
+	
+	printf("Enter number number of colums : ");
+	scanf("%d",&iValue2);
+	
+	Pattern(iValue1,iValue2);
+	printf("\n");
+	
+	return 0;
+}
