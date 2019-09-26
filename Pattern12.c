1debdca107d21fdb3d5c73cb7b0e34eb6bb0fbfa
Mangesh Prakash Jadhav <42151779+mangesh1409@users.noreply.github.com>
2019-09-27 00:54:06 +0530

    Update Pattern12.c

--- Pattern12.c
+++ Pattern12.c
@@ -1,6 +1,7 @@
 /*  Accept number from user and display below pattern
     Input= 5								               
-    Output= 5 4 3 2 1
+    Output= 
+    				5 4 3 2 1
     				5 4 3 2 1
     				5 4 3 2 1
     				5 4 3 2 1	
