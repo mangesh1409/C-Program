c820dcc0db670a68aa7edb9c415241936bae7f5d
Mangesh Prakash Jadhav <42151779+mangesh1409@users.noreply.github.com>
2019-10-02 23:01:13 +0530

    Update DiffSumEvenOdd.c

--- DiffSumEvenOdd.c
+++ DiffSumEvenOdd.c
@@ -1,8 +1,9 @@
-/*    Accept N numbers from user and return difference between summation of even elements and summation of odd elements.
-	  Input :
-				N : 6
-				Elements : 85  66  3  80  93  88
-				Output : 53 (234 - 181)						*/
+/* Accept N numbers from user and return difference between 
+   summation of even elements and summation of odd elements.
+      Input :
+		N : 6
+		Elements : 85  66  3  80  93  88
+		Output : 53 (234 - 181)				*/
 
 #include<stdio.h>
 #include<stdlib.h>
